32fc928563dfc49dd5186183bcf8351644347a32
ASpoerndle <aspoerndle@elon.edu>
2026-03-16 12:14:00 -0400

    Update Subscriber.py

--- RobotController/Subscriber.py
+++ RobotController/Subscriber.py
@@ -21,14 +21,16 @@ def main(args=None):
     rclpy.init(args=args)
 
     minimal_subscriber = MinimalSubscriber()
-
-    rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+     try:
+        rclpy.spin(minimal_subscriber)
+    except KeyboardInterrupt:
+        
+        # Destroy the node explicitly
+        # (optional - otherwise it will be done automatically
+        # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 
 if __name__ == '__main__':
