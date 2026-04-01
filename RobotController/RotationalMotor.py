6b8ae9804791bbc9b66561d5fd50436ed4ab4288
ASpoerndle <aspoerndle@elon.edu>
2026-04-01 19:51:35 -0400

    Added move by distance
    The following update to RotationalMotor.py should allow moving by distance using the move() method. It takes in the distance desired to travel, the speed to travel, and the starting position of the encoder.

--- RobotController/RotationalMotor.py
+++ RobotController/RotationalMotor.py
@@ -6,12 +6,14 @@ from adafruit_pca9685 import PCA9685
 
 import Jetson.GPIO as GPIO
 import time
+import math
 class RotationalMotor():
   I2C_ADDR = 0x30
   I2C_BUS = 1
   positions = [0,0,0,0,0,0,0,0]
   velocities = [0,0,0,0,0,0,0,0]  
-  
+  WHEELDIAMETER = .144
+  WHEELC = WHEELDIAMETER * math.pi
   #left is more pos, right is more neg
   def __init__(self, pca, pin, side, enc, fVal):
     self.motor = WheelMotor(pca,pin,side)
@@ -71,13 +73,29 @@ class RotationalMotor():
     #TODO - if current Pos < forward + 90, rotate right
   def getCurrentPosition(self):
       return RotationalMotor.positions[self.enc]
+
+  #input distance in m, speed -1.0 to 1.0
+  def move(self,distance,speed, startPos):
+    rev_dis = distance / RotationalMotor.WHEELC
+    count_dis = rev_dis * 8192 + startPos
+    self.read_octoquad()
+    current_pos = self.getCurrentPosition()
+    if(count_dis > current_pos and distance > 0):
+      self.motor.move_motor(speed)
+      return True
+    elif(count_dis < current_pos and distance < 0):
+      self.motor.move_motor(-speed)
+      return True
+    else:
+      self.motor.move_motor(0)
+      return False
   # OctoQuad default settings
   def read_octoquad(self):
     with SMBus(RotationalMotor.I2C_BUS) as bus:
         addr = RotationalMotor.I2C_ADDR
         # Read all 8 channels (32 bytes total) starting from register 0x00
         all_positions = bus.read_i2c_block_data(addr, 0x1C, 32)
-        all_velocities = bus.read_i2c_block_data(addr, 0x2C,16)
+        all_velocities = bus.read_i2c_block_data(addr, 0x3C,16)
         
         # Unpack into a list of 8 integers
         # '<8i' means 8 little-endian signed integers
@@ -109,10 +127,20 @@ try:
     #     val = rotMotor.adjustForward()
     #     print(rotMotor.getCurrentPosition())
     # print("finshed")
+    print("Adjusting forward...")
     while(val):
       val = rotMotor.adjustForward()
     val = rotMotor.rotate(90)
+    print("Forward adjustment complete!")
+    time.sleep(1)
+    print("Rotating Motor 90 degrees...")
     while(val):
       val = rotMotor.rotate(90)
+    print("Rotation complete!")  
+    # startPos = rotMotor.getCurrentPosition()
+    # val = rotMotor.move(0.5,.1,startPos)
+    # while(val):
+    #   val = rotMotor.move(0.5,.1)
+    print("Finished protocol!")
 except KeyboardInterrupt:
     rotMotor.stopMotor()
