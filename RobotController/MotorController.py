af08241e5396ac84fe232b99517b0faf31f1c59f
Aidan Spoerndle <aspoerndle@elon.edu>
2026-04-07 11:27:27 -0400

    nothing

--- RobotController/MotorController.py
+++ RobotController/MotorController.py
@@ -1,146 +1,73 @@
-import time
-from Motor import WheelMotor
-from RotationalMotor import RotationalMotor
 import board
 from adafruit_pca9685 import PCA9685
-
 import Jetson.GPIO as GPIO
 import time
 
-
 class MotorController():
-
     def __init__(self):
-        
-        pin1 = 31
-        pin2 = 29
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-        GPIO.setup(pin1, GPIO.IN)  # pin 31
-        GPIO.setup(pin2, GPIO.IN) # pin 29
-
-
-        input1 = GPIO.input(pin1)  # Read input
-        input2 = GPIO.input(pin2)
-
-
-
-
-
+        GPIO.setmode(GPIO.BOARD)
+        # PCA9685 Setup
         i2c = board.I2C()
-        pca = PCA9685(i2c)
-        pca.frequency = 50
+        self.pca = PCA9685(i2c)
+        self.pca.frequency = 50
 
-        self.wheel_motor_list = []
-        self.rotational_motor_list = []
-        #pin_list_wheel = [[11, 'l'],[10,'l'],[13,'r'],[15,'r']]
-        pin_list_rotational = [[2,"l",0,0],[3,"l",1,0],[4, "l",2,0],[6,"l",3,0],[11,'l',6,0],[10,'l',5,0],[13,'r',4,0],[15,'r',7,0]]
-        #print("readying wheel motors...")
-        #for i in pin_list_wheel:
-        #    motor = WheelMotor(pca,i[0], i[1])
-        #    self.wheel_motor_list.append(motor)
-        print("readying rotational motors...")
-        for i in pin_list_rotational:
-            motor = RotationalMotor(pca, i[0],i[1],i[2],i[3])
-            self.rotational_motor_list.append(motor)
-        
-    def moveWheels(self, i):
-        print("moving forward")
-        if(i < -1 or i > 1):
-            print("ERR: invalid input")
-            return
-        for motor in self.wheel_motor_list:
-            motor.move_motor(i)
-    def adjustForward(self, resetWheels):
-        cond1 = True
-        cond2 = False
-        isMotorAligned1 = isMotorAligned2 = isMotorAligned3 = isMotorAligned4 = False
-        stopCond = False
-        if(resetWheels):
-            motor1,motor2,motor3,motor4 = self.rotational_motor_list[4:8]
-        else:
-            motor1,motor2,motor3,motor4 = self.rotational_motor_list[0:4]
-        while(not stopCond):
-            if(not isMotorAligned1):
-                cond1 = motor1.adjustForward()
-                isMotorAligned1 = cond1
-            if(not isMotorAligned2):
-                cond2 = motor2.adjustForward()
-                isMotorAligned2 = cond2
-            if(not isMotorAligned3):
-                isMotorAligned3 = motor3.adjustForward()
-            if(not isMotorAligned4):
-                isMotorAligned4 = motor4.adjustForward()
-            stopCond = isMotorAligned2 and isMotorAligned1 and isMotorAligned3 and isMotorAligned4
-        self.stopMotors()
-    def rotate(self,angle,speed, whichMotor):
-        isMotorAligned1 = isMotorAligned2 = isMotorAligned3 = isMotorAligned4 = False
+        # Define motors: [PCA_Pin, Side, Encoder_Ch, forward_offset]
+        # Using your provided indices
+        self.rotational_motors = []
+        setup_data = [
+            [2,"l",0,0], [3,"l",1,0], [4,"l",2,0], [6,"l",3,0], # Front/Aux
+            [11,'l',6,0], [10,'l',5,0], [13,'r',4,0], [15,'r',7,0] # Main Drive Group
+        ]
 
-        stopCond = False
-        if(whichMotor == "w"):
-            motor1,motor2,motor3,motor4 = self.rotational_motor_list[4:8]
-            global1 = (motor1.getCurrentPosition()/8192)*360
-            global2 =  (motor2.getCurrentPosition()/8192)*360
-            global3 =  (motor3.getCurrentPosition()/8192)*360
-            global4 =  (motor4.getCurrentPosition()/8192)*360
-            angle1 = self.calculateRelativeAngle(angle,global1)
-            angle2 = self.calculateRelativeAngle(angle,global2)
-            angle3 = self.calculateRelativeAngle(angle,global3)
-            angle4 = self.calculateRelativeAngle(angle,global4)
-            print(str(motor4.getCurrentPosition()) + "CP")
-            print(angle1,angle2,angle3,angle4)
-        else:
-            angle1=angle2=angle3=angle4=angle
-            motor1,motor2,motor3,motor4 = self.rotational_motor_list[0:4]
-        while(not stopCond):
-            if(not isMotorAligned1):
-                isMotorAligned1 = motor1.rotateForward(angle1,-speed)
-            if(not isMotorAligned2):
-                isMotorAligned2 = motor2.rotateForward(angle2,speed)
-            if(not isMotorAligned3):
-                isMotorAligned3 = motor3.rotateForward(angle3,speed)
-            if(not isMotorAligned4):
-                
-                isMotorAligned4 = motor4.rotateForward(angle4,speed)
-            stopCond = isMotorAligned1 and isMotorAligned2 and isMotorAligned3 and isMotorAligned4
-            #if(whichMotor == "w"):
-             #   stopCond = isMotorAligned1 or isMotorAligned2 or isMotorAligned3 or isMotorAligned4
-        self.stopMotors()
-    def calculateRelativeAngle(self,target,current):
-        relative = target-current
-        relative = (relative+180) % 360 - 180
+        print("Initializing Rotational Motors...")
+        for data in setup_data:
+            m = RotationalMotor(self.pca, data[0], data[1], data[2], data[3])
+            self.rotational_motors.append(m)
+
+    def calculateRelativeAngle(self, target, current):
+        """Finds the shortest distance between two angles on a 360 circle."""
+        relative = (target - current + 180) % 360 - 180
         return relative
-    def moveDistance(self, distance,speed):
-        rev_dis = distance / .144
-        degree_dis = rev_dis *360
-        print(degree_dis)
-        self.rotate(degree_dis,speed,"w")
+
+    def rotate(self, target_angle, speed, group="w"):
+        """
+        Rotates a group of motors to a global target angle.
+        'w' for drive wheels (4-7), else (0-3).
+        """
+        if group == "w":
+            motors = self.rotational_motors[4:8]
+        else:
+            motors = self.rotational_motors[0:4]
+
+        # Calculate individual relative steps needed for each wheel
+        relative_angles = []
+        for m in motors:
+            current_global = (m.getCurrentPosition() / 8192) * 360
+            rel = self.calculateRelativeAngle(target_angle, current_global)
+            relative_angles.append(rel)
+
+        # Main Control Loop
+        while True:
+            all_aligned = True
+            for i, motor in enumerate(motors):
+                # We pass the relative angle once; internal logic handles the rest
+                is_done = motor.rotateForward(relative_angles[i], speed)
+                if not is_done:
+                    all_aligned = False
+            
+            if all_aligned:
+                break
+            
+            time.sleep(0.01) # Yield for I2C bus
+
+        self.stopMotors()
+
     def stopMotors(self):
-        for motor in self.rotational_motor_list:
+        for motor in self.rotational_motors:
             motor.stopMotor()
+
     def __del__(self):
-        for motor in self.wheel_motor_list:
-            motor.move_motor(0)
-        for motor in self.rotational_motor_list:
-            motor.stopMotor()
-        time.sleep(2)
-        print("finished")
-   
-#try:       
+        self.stopMotors()
+        GPIO.cleanup()
 mc = MotorController()
-time.sleep(3)
-#mc.adjustForward(False)
-#mc.adjustForward(True)
-#time.sleep(1)
-#mc.rotate(-45,.1,"r")
-#time.sleep(1)
-#mc.adjustForward(False)
-#time.sleep(1)
-#mc.moveDistance(0.5,0.1)
-#mc.rotate(-45,.3,"w")
-#time.sleep(1)
-while True:
-    rot = input("To which degree")
-    mc.rotate(int(rot),0.3,"w")
-#except KeyboardInterrupt:
- #   mc.stopMotors()
+mc.rotate(10,0.3,"w")
--- RobotController/RotationalMotor.py
+++ RobotController/RotationalMotor.py
@@ -1,216 +1,117 @@
 import struct
 from smbus2 import SMBus
-from Motor import WheelMotor
-import board
-from adafruit_pca9685 import PCA9685
-
-import Jetson.GPIO as GPIO
 import time
 import math
+from Motor import WheelMotor
+
 class RotationalMotor():
-  I2C_ADDR = 0x30
-  I2C_BUS = 1
-  positions = [0,0,0,0,0,0,0,0]
-  velocities = [0,0,0,0,0,0,0,0]  
-  WHEELDIAMETER = .144
-  WHEELC = WHEELDIAMETER * math.pi
-  #left is more pos, right is more neg
-  def __init__(self, pca, pin, side, enc, fVal):
-    self.motor = WheelMotor(pca,pin,side)
-    self.enc = enc
-    if(side == "r"):
-        self.polarity = -1
-    else:
-        self.polarity = 1
-    self.fVal = fVal
-    self.currentCount = fVal
-    self.read_octoquad()
-
-  #Returns T/F based on if it's off-centered, put a while loop in MotorController class so it can adjust all motors at once
-  def adjustForward(self):
-      self.read_octoquad()
-      
-      currentPos = RotationalMotor.positions[self.enc]
-      if(currentPos > self.fVal - 10 and currentPos < self.fVal + 10):
-         self.motor.move_motor(0)
-         return True
-      elif(currentPos < self.fVal):
-        self.motor.move_motor(.1)
-        print("rotate left for center")
-        return False
-      else:
-          self.motor.move_motor(-.1)
-          print("rotate right for center")
-          return False
-  #right is neg, left is pos
-  def setMotorSpeed(self,speed):
-      self.motor.move_motor(speed)
-  def rotate(self, angle, speed):
-    speed = abs(speed)
-    self.read_octoquad()
-    if(self.polarity > 0):
-        if(angle < 0):
-            cond = self.rotateLeft(angle, speed)
-        if(angle > 0):
-        #true = True
-            cond = self.rotateRight(angle , speed)
-    #else:
-      #self.adjustForward()
-        if(angle == 0):
-            cond = True
-    else:
-        if(angle > 0):
-            cond = self.rotateLeft(angle,speed)
-        if(angle < 0):
-            
-            cond = self.rotateRight(angle,speed)
-        if(angle == 0):
-            cond = True
-    if(cond):
-      self.currentCount = self.getCurrentPosition()
-      return True
-    else:
-      return False
-  def rotateForward(self,angle,speed):
-        speed = abs(speed)
+    I2C_ADDR = 0x30
+    I2C_BUS = 1
+    positions = [0]*8
+    velocities = [0]*8
+    
+    def __init__(self, pca, pin, side, enc, fVal):
+        self.motor = WheelMotor(pca, pin, side)
+        self.enc = enc
+        self.polarity = -1 if side == "r" else 1
+        self.fVal = fVal
+        self.currentCount = 0 
+        self.is_moving = False
         self.read_octoquad()
-        self.currentCount = 0
-        if(self.polarity > 0):
-            if(angle < 0):
-                cond = self.rotateLeft(angle, speed)
-            if(angle > 0):
-                cond = self.rotateRight(angle , speed)
-            if(angle == 0):
-                cond = True
-        else:
-            if(angle > 0):
-                cond = self.rotateLeft(angle,speed)
-            if(angle < 0):
-            
-                cond = self.rotateRight(angle,speed)
-            if(angle == 0):
-                cond = True
-        if(cond):
-            self.currentCount = self.getCurrentPosition()
-            return True
-        else:
-            return False
 
-  def stopMotor(self):
-      self.motor.move_motor(0)
-  def rotateLeft(self, angle,  speed):
-    current = self.getCurrentPosition()
-    new_pos = ((angle * 1024)/45)*self.polarity  + self.currentCount
-    if(self.polarity > 0):
+    def read_octoquad(self):
+        try:
+            with SMBus(RotationalMotor.I2C_BUS) as bus:
+                all_positions = bus.read_i2c_block_data(RotationalMotor.I2C_ADDR, 0x1C, 32)
+                RotationalMotor.positions = list(struct.unpack('<8i', bytes(all_positions)))
+        except Exception as e:
+            print(f"I2C Read Error: {e}")
 
-        if(current > new_pos):
-            self.motor.move_motor(-speed)
-            return False
-    else:
-        if(current < new_pos):
+    def getCurrentPosition(self):
+        return RotationalMotor.positions[self.enc]
 
-            self.motor.move_motor(-speed)
-            return False
-    
-    self.motor.move_motor(0)
-    return True
-    #TODO - if current Pos > forward - 90, rotate left
-    
-  def rotateRight(self, angle, speed):
-    new_pos = ((angle * 1024)/45) * self.polarity + self.currentCount
-    current = self.getCurrentPosition()
-    print("CC: " + str(current))
-    print("NP: " + str(new_pos))
-    print("Encoder: " + str(self.enc) + "has speed of: " + str(speed*self.polarity))
-
-
-    if self.polarity > 0:
-      if(current < new_pos):
-     # print("moving motor...")
-        self.motor.move_motor(speed)
-        return False
-    else:
-      print(f"Right Motor {self.enc}: Cur {current} | Target {new_pos}")
-      if(current < new_pos):
-        self.motor.move_motor(speed)
-        return False
-    self.motor.move_motor(0)
-    return True
-    #TODO - if current Pos < forward + 90, rotate right
-  def getCurrentPosition(self):
-      return RotationalMotor.positions[self.enc]
-
-  #input distance in m, speed -1.0 to 1.0
-  def move(self,distance,speed):
-    rev_dis = distance / RotationalMotor.WHEELC
-    degree_dis = rev_dis * 360
-    
-    if(count_dis > self.current_count and distance > 0):
-      self.rotate(degree_dis, speed)
-      return True
-    elif(count_dis < self.current_count and distance < 0):
-      self.rotate(degree_dis,speed)
-      return True
-    else:
-        self.current_count = self.getCurrentPosition()
-        return False
-  # OctoQuad default settings
-  def read_octoquad(self):
-    with SMBus(RotationalMotor.I2C_BUS) as bus:
-        addr = RotationalMotor.I2C_ADDR
-        # Read all 8 channels (32 bytes total) starting from register 0x00
-        all_positions = bus.read_i2c_block_data(addr, 0x1C, 32)
-        all_velocities = bus.read_i2c_block_data(addr, 0x3C,16)
+    def rotateForward(self, angle, speed):
+        """
+        The entry point for movement. Captures start position once
+        and delegates to the internal directional methods.
+        """
+        self.read_octoquad()
         
-        # Unpack into a list of 8 integers
-        # '<8i' means 8 little-endian signed integers
-        RotationalMotor.velocities = struct.unpack('<8h', bytes(all_velocities))
-        RotationalMotor.positions = struct.unpack('<8i', bytes(all_positions))
-        print(RotationalMotor.positions) 
-        # for i, val in enumerate(positions):
-        #     channels[i] = val
-    time.sleep(.01)
-        # return position, velocity
-#Ideal position is -13
+        # Capture the starting point only when a new move begins
+        if not self.is_moving:
+            self.currentCount = self.getCurrentPosition()
+            self.is_moving = True
 
-"""
-TESTING GROUND FOR ROTATIONAL MOTOR
+        # Calculate absolute target in encoder counts
+        target_counts = (angle * 1024) / 45
+        new_pos = (target_counts * self.polarity) + self.currentCount
+        
+        # Delegate based on angle sign
+        if angle >= 0:
+            done = self._rotateRight(new_pos, abs(speed))
+        else:
+            done = self._rotateLeft(new_pos, abs(speed))
+            
+        if done:
+            self.is_moving = False # Reset for the next command
+        return done
 
-given a pca address, pin value, and a side
-"""
-"""try:
-    i2c = board.I2C()
-    pca = PCA9685(i2c)
-    pca.frequency = 50
-    pin = 11
-    side = "l"
-    idealfVal = 0
-    channel = 6
-    rotMotor = RotationalMotor(pca,pin,side,channel,idealfVal)
-    val = rotMotor.adjustForward()
-    # while(val):
-    #     val = rotMotor.adjustForward()
-    #     print(rotMotor.getCurrentPosition())
-    # print("finshed")
-    print("Adjusting forward...")
-    while(val):
-      val = rotMotor.adjustForward()
-    print("Forward adjustment complete!")
-    time.sleep(1)
-    print("Rotating Motor 90 degrees...")
-    val = True
-    #rotMotor.setMotorSpeed(-.2)
-    while(val):
-           
-        val = rotMotor.rotate(-90,.1)
-    val = True
-    while(val):
-        val = rotMotor.adjustForward()
-    print("Rotation complete!")  
-    # startPos = rotMotor.getCurrentPosition()
-    # val = rotMotor.move(0.5,.1,startPos)
-    # while(val):
-    rotMotor.stopMotor()
-    #   val = rotMotor.move(0.5,.1)
-except KeyboardInterrupt:
-    rotMotor.stopMotor()"""
+    def _rotateRight(self, target, speed):
+        current = self.getCurrentPosition()
+        tolerance = 15 # Stops the "shaking" jitter
+
+        if abs(current - target) < tolerance:
+            self.motor.move_motor(0)
+            return True
+
+        if self.polarity > 0:
+            if current < target:
+                self.motor.move_motor(speed)
+                return False
+        else:
+            # Negative Polarity: Move while current is GREATER (less negative) than target
+            if current > target:
+                self.motor.move_motor(speed)
+                return False
+
+        self.motor.move_motor(0)
+        return True
+
+    def _rotateLeft(self, target, speed):
+        current = self.getCurrentPosition()
+        tolerance = 15
+
+        if abs(current - target) < tolerance:
+            self.motor.move_motor(0)
+            return True
+
+        if self.polarity > 0:
+            if current > target:
+                self.motor.move_motor(-speed)
+                return False
+        else:
+            # Negative Polarity: Move while current is LESS (more positive) than target
+            if current < target:
+                self.motor.move_motor(-speed)
+                return False
+
+        self.motor.move_motor(0)
+        return True
+
+    def adjustForward(self):
+        self.read_octoquad()
+        currentPos = self.getCurrentPosition()
+        tolerance = 10
+        
+        if abs(currentPos - self.fVal) < tolerance:
+            self.motor.move_motor(0)
+            return True
+        
+        # Simple proportional-ish correction
+        speed = 0.1 if currentPos < self.fVal else -0.1
+        self.motor.move_motor(speed)
+        return False
+
+    def stopMotor(self):
+        self.motor.move_motor(0)
+        self.is_moving = False
