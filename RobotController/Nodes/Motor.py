861283a14c3c8c2c8398560e3f0f34fbc34d5e2a
ASpoerndle <aspoerndle@elon.edu>
2026-04-24 11:27:24 -0400

    Update Motor.py

--- RobotController/Nodes/Motor.py
+++ RobotController/Nodes/Motor.py
@@ -1,75 +1,52 @@
 import board
 import time
 from adafruit_pca9685 import PCA9685
-'''
-i2c = board.I2C()
-pca = PCA9685(i2c)
-pca.frequency = 50
-
-# Use the CHANNEL, not the servo object
-motor_channel = pca.channels[15]
-motor_channel2 = pca.channels[14]
-print("readying device")
-
-motor_channel.duty_cycle = 5400
-motor_channel2.duty_cycle = 5400
-time.sleep(1)
-
-for i in range(5000,6050,5):
-    motor_channel.duty_cycle = i
-    motor_channel2.duty_cycle = i
-    print(i)
-    time.sleep(0.05)
-
-for x in range(6050,5000,-5):
-    motor_channel2.duty_cycle = x
-    motor_channel.duty_cycle = x
-    print(x)
-    time.sleep(0.05)
-'''
 
 
 class WheelMotor:
     default_f = 5400
     default_r = 5000
     current_duty = 5200
+
+    """
+    Method: forward_motion(speed)
+    Purpose: Adjusts the motors duty cycle proportionally to an inputted float speed so that the 
+             robot can move forward
+    """
     def forward_motion(self, speed):
-        #print("f")
+        
         new_speed = WheelMotor.default_f + (1300 * speed)
-        #for i in range(WheelMotor.default_f, int(new_speed),5):
-         #   self.motor.duty_cycle = i
-          #  time.sleep(0.05)
-        #print("new FC: ", new_speed)
         self.motor.duty_cycle = int(new_speed)
         WheelMotor.current_duty = int(new_speed)
+    """
+    Method: backward_motion(speed)
+    Purpose: Adjusts the motors duty cycle proportionally to an inputted float speed so that
+             the robot can move backward
+    """
     def backward_motion(self,speed):
-        #print("r")
+        
         new_speed = WheelMotor.default_r + (1300 * speed)
-        #for i in range(WheelMotor.default_r, int(new_speed), -5):
-         #   self.motor.duty_cycle = i
-          #  time.sleep(0.05)
-        #print("new DC: ", new_speed)
         self.motor.duty_cycle = int(new_speed)
         WheelMotor.current_duty = int(new_speed)
-    def zero_from_forward(self):
-        #for i in range(WheelMotor.current_duty, WheelMotor.default_f,-5):
-         #   self.motor.duty_cycle = i
-          #  time.sleep(0.05)
+    """
+    Method: zero_motion()
+    Purpose: sets the motor duty cycle to a motionless speed (5200 mHz)
+    """
+    def zero_motion(self):
         self.motor.duty_cycle = 5200
         WheelMotor.current_duty = 5200
 
-    def zero_from_backward(self):
-        #for i in range(WheelMotor.current_duty, WheelMotor.default_r,5):
-         #   self.motor.duty_cycle = i
-          #  time.sleep(0.05)
-        self.motor.duty_cycle = 5200
-        WheelMotor.current_duty = 5200
-
+    
     def __init__(self,pca, pin,side):
         self.motor = pca.channels[pin]
         self.motor.duty_cycle = 5200
         self.side = side
-        print("New Motor Added")
+       
+    """
+    Method: move_motor(speed)
+    Purpose: takes a float input {speed} and calls the proper method depending on which side the motor is on
+             and whether the inputted speed is positive or negative
+    """
     def move_motor(self, speed):
         if(speed > 0 and speed <= 1):
             if(self.side == 'r'):
@@ -86,8 +63,5 @@ class WheelMotor:
             self.backward_motion(speed)
             current_speed = speed
         elif(speed == 0):
-            if(WheelMotor.current_duty > WheelMotor.default_f):
-                self.zero_from_forward()
-            else:
-                self.zero_from_backward()
+            self.zero_motion()
 
