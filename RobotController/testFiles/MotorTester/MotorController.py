3b786d7676c2c21851b5c7087fdbf9a3a4dbfbaf
Spoon <aspoerndle@elon.edu>
2026-05-02 13:42:19 -0400

    added wheel controller class

--- RobotController/testFiles/MotorTester/MotorController.py
+++ RobotController/testFiles/MotorTester/MotorController.py
@@ -1,6 +1,3 @@
-import time
-
-
 from RotationalMotor import RotationalMotor
 from PodController import PodController
 from WheelController import WheelController
@@ -101,114 +98,13 @@ class MotorController():
         return
 
 
-    """
-    Method: rampSpeedPos(motor, ticks, speed)
-    Purpose: taking in speed input and current tick count, the method ramps the speed up and down. For moving forward
-    """
-    def rampSpeedPos(self,motor,ticks,speed):
-        if (motor.getCurrentPosition() < 100 ):
-            speed = 0.5
-        elif (abs(motor.getCurrentPosition()) < abs(3 * ticks // 8) and abs(ticks) > 1000 and speed < .8):
-            speed += 0.01
-        elif (abs(motor.getCurrentPosition()) > abs(5 * ticks // 8) and abs(ticks) > 1000 and speed > 0.3):
-            speed -= 0.01
-        return speed
-    """
-    Method: rampSpeedNeg(motor,ticks,speed)
-    Purpose: taking in speed input and current tick count, the method ramps the speed up and down. For moving backward
-    """
-    def rampSpeedNeg(self,motor,ticks,speed):
-        if (motor.getCurrentPosition() > -100):
-            speed = -.5
-        elif (motor.getCurrentPosition() > 3 * ticks // 8 and ticks < 1000 and speed > -.8):
-            speed -= 0.01
-        elif (motor.getCurrentPosition() < 5 * ticks // 8 and ticks < 1000 and speed < -.3):
-            speed += 0.01
-        return speed
-
-    """
-    Method: checkRotateForward()
-    Purpose: calls the motors rotateForward() method to check if the motor has reached it's intended destination
-    """
-    def checkRotateForward(self,motor,ticks,speed,isBack,debug):
-        
-        if(debug):
-            print(f"Ticks: {ticks} | Speed: {speed}")
-        return motor.rotateForward(ticks, speed, isBack, debug)
 
     def rotateForward(self, ticks,debug,inPlace):
         self.wheelController.rotateForward(ticks,debug,inPlace)
-        # polar = 0
-        # if(ticks < 0):
-        #     polar = -1
-        #     speed = -.5
-        #     isBack = True
-        # else:
-        #     polar = 1
-        #     speed = .5
-        #     isBack = False
-        # #Alter logic for determing ramp up and ramp down
-        # MotorList = self.rotational_motor_list[4:8].copy()
-        # stopCond = False
-        # if(debug):
-        #     print(f"Polar: {polar}")
-        #
-        # MotorList[0].resetEncoder()
-        # time.sleep(0.05)
-        # if(debug):
-        #     print(f"Reset encoder {MotorList[0]}")
-        #
-        # while (not stopCond):
-        #
-        #     for i,motor in enumerate(MotorList):
-        #
-        #         if(i > 1):
-        #             isThere = self.checkRotateForward(motor,-ticks*inPlace,speed*inPlace,isBack,debug)
-        #         else:
-        #             isThere = self.checkRotateForward(motor,ticks,speed,isBack,debug)
-        #
-        #         if(isThere):
-        #             MotorList.pop(i)
-        #             break
-        #         if(debug):
-        #             print(f"Loop: {i} | Ticks {ticks}")
-        #     stopCond = len(MotorList) <= 3
-        #
-        #     if(debug):
-        #         print(f'Ticks: {ticks} + Speed: {speed}')
-        #
-        #     if(polar > 0):
-        #         speed = self.rampSpeedPos(MotorList[0],ticks,speed)
-        #     else:
-        #         speed = self.rampSpeedNeg(MotorList[0],ticks,speed)
-        #     time.sleep(0.02)
-        #
-        # self.stopMotors()
 
-    def checkRotate(self,motor,angle,speed,debug):
-        return motor.rotate(angle, speed, debug)
 
     def rotatePods(self, angle,debug):
         self.podController.rotatePods(angle,debug)
-        # speed = 0.75
-        # if angle > 90 or angle < -90:
-        #     print(f"ROTATION ERR: Angle of {angle} degrees will cause wire damage")
-        #     return
-        #
-        #
-        # stopCond = False
-        # MotorList = self.rotational_motor_list[0:4].copy()
-        #
-        # while (not stopCond):
-        #     for i,motor in enumerate(MotorList):
-        #         isAligned = self.checkRotate(motor,angle,speed,debug)
-        #         if(isAligned):
-        #             MotorList.pop(i)
-        #
-        #     stopCond = len(MotorList) == 0
-        #     time.sleep(0.02)
-        # self.stopMotors()
-        #
 
 
     def moveDistance(self, distance, debug,isZero):
@@ -222,39 +118,20 @@ class MotorController():
         if(isZero):
             for i in range(6,8):
                 self.rotational_motor_list[i].switchPolarity()
-            self.rotateForward(ticks,debug,-1)
+            self.wheelController.rotateForward(ticks,debug,-1)
             for i in range(6,8):
                 self.rotational_motor_list[i].switchPolarity()
         else:
             if(debug):
                 print(f"Rotating forward...")
-            self.rotateForward(ticks, debug,1)
+            self.wheelController.rotateForward(ticks,debug,1)
+
 
     def horizontalMode(self,debug):
-        self.rotatePods(-90,debug)
+        self.podController.rotatePods(-90,debug)
             
     def rotateXMotors(self,angle,motorList,debug):
         self.podController.rotateXMotors(angle,motorList,debug)
-        # speed = 0.75
-        # motors = []
-        # for i in range(len(motorList)):
-        #     motors.append(self.rotational_motor_list[motorList[i]])
-        #
-        # if angle > 90 or angle < -90:
-        #     print(f"ROTATION ERR: Angle of {angle} degrees is will cause wire damage")
-        #     return
-        #
-        # stopCond = False
-        #
-        # while (not stopCond):
-        #
-        #     for i,motor in enumerate(motors):
-        #         isRotated = self.checkRotate(motor,angle,speed,debug)
-        #         if(isRotated):
-        #             motors.pop(i)
-        #     stopCond = len(motors) == 0
-        #     time.sleep(0.02)
-        # self.stopMotors()
 
     def rotateAllMotors(self,speed,angle,debug):
         speed = 0.75
@@ -267,14 +144,14 @@ class MotorController():
         while(not stopCond):
             for i,motor in enumerate(motors):
                 if(i<4):
-                    isRotated = self.checkRotate(motor,angle,speed,debug)
+                    isRotated = self.podController.checkRotate(motor,angle,speed,debug)
                     if(isRotated):
                         motors.pop(i)
                 if(i>3):
                     if(i>5):
-                        isThere = self.checkRotateForward(motor,ticks,speed,isBack,debug)
+                        isThere = self.wheelController.checkRotateForward(motor,ticks,speed,isBack,debug)
                     else:
-                        isThere = self.checkRotateForward(motor,-ticks,speed,isBack,debug)
+                        isThere = self.wheelController.checkRotateForward(motor,-ticks,speed,isBack,debug)
                     if(isThere):
                         motors.pop(i)
             stopCond = len(motors) < 4
@@ -327,7 +204,7 @@ class MotorController():
         self.adjustForward(debug)
         self.moveDistance(dis,debug,False)
         time.sleep(1)
-        self.horizontalMode(debug,False)
+        self.horizontalMode(debug)
         time.sleep(1)
         self.moveDistance(-dis,debug,False)
         time.sleep(1)
