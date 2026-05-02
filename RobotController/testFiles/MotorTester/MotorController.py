865daff9269a35115eeee03642a91515c35ecc17
Spoon <aspoerndle@elon.edu>
2026-05-02 13:22:33 -0400

    a

--- RobotController/testFiles/MotorTester/MotorController.py
+++ RobotController/testFiles/MotorTester/MotorController.py
@@ -67,13 +67,14 @@ class MotorController():
     Purpose: For the TeleOp controller, sets the robot to "Turn Mode", allowing it to turn in place
     """
     def teleTurn(self):
-        self.rotateXMotors(45, [2,0], False)
-
-        self.rotateXMotors(-45, [1,3], False)
-        for i,motor in enumerate(self.rotational_motor_list[4:8]):
-            if(i<2):
-                if(motor.getPolarity() == 1):
-                    motor.switchPolarity()
+        self.podController.teleTurn()
+        # self.rotateXMotors(45, [2,0], False)
+        #
+        # self.rotateXMotors(-45, [1,3], False)
+        # for i,motor in enumerate(self.rotational_motor_list[4:8]):
+        #     if(i<2):
+        #         if(motor.getPolarity() == 1):
+        #             motor.switchPolarity()
     """
     Method: teleMoveTurn(Speed)
     Purpose: For the TeleOp controller, allows for the robot to turn in place in "Turn Mode"
@@ -91,14 +92,15 @@ class MotorController():
     Purpose: For the TeleOp controller, allows the pod motors to rotate together while maintaining the same heading
     """
     def teleRotate(self,speed):
-        MAX_ROTATE = 60
-        for i,motor in enumerate(self.rotational_motor_list[0:4]):
-            if((motor.getCurrentAngle() < MAX_ROTATE and speed < 0) or (motor.getCurrentAngle() > -MAX_ROTATE and speed > 0)):
-                if(abs(motor.getCurrentAngle()) < 90):
-                    print(f"Angle: {motor.getCurrentAngle()}, Speed: {speed}")
-                    motor.setSpeed(speed)
-            else:
-                motor.setSpeed(0)
+        self.podController.teleRotate(speed)
+        # MAX_ROTATE = 60
+        # for i,motor in enumerate(self.rotational_motor_list[0:4]):
+        #     if((motor.getCurrentAngle() < MAX_ROTATE and speed < 0) or (motor.getCurrentAngle() > -MAX_ROTATE and speed > 0)):
+        #         if(abs(motor.getCurrentAngle()) < 90):
+        #             print(f"Angle: {motor.getCurrentAngle()}, Speed: {speed}")
+        #             motor.setSpeed(speed)
+        #     else:
+        #         motor.setSpeed(0)
 
     """
     Method: adjustForward(debug)
@@ -203,25 +205,26 @@ class MotorController():
         return motor.rotate(angle, speed, debug)
 
     def rotatePods(self, angle,debug):
-        speed = 0.75
-        if angle > 90 or angle < -90:
-            print(f"ROTATION ERR: Angle of {angle} degrees will cause wire damage")
-            return
-        
-        
-        stopCond = False
-        MotorList = self.rotational_motor_list[0:4].copy()
-        
-        while (not stopCond):
-            for i,motor in enumerate(MotorList):
-                isAligned = self.checkRotate(motor,angle,speed,debug)
-                if(isAligned):
-                    MotorList.pop(i)
-            
-            stopCond = len(MotorList) == 0
-            time.sleep(0.02)
-        self.stopMotors()
-        
+        self.podController.rotatePods(angle,debug)
+        # speed = 0.75
+        # if angle > 90 or angle < -90:
+        #     print(f"ROTATION ERR: Angle of {angle} degrees will cause wire damage")
+        #     return
+        #
+        #
+        # stopCond = False
+        # MotorList = self.rotational_motor_list[0:4].copy()
+        #
+        # while (not stopCond):
+        #     for i,motor in enumerate(MotorList):
+        #         isAligned = self.checkRotate(motor,angle,speed,debug)
+        #         if(isAligned):
+        #             MotorList.pop(i)
+        #
+        #     stopCond = len(MotorList) == 0
+        #     time.sleep(0.02)
+        # self.stopMotors()
+        #
 
 
     def moveDistance(self, distance, debug,isZero):
