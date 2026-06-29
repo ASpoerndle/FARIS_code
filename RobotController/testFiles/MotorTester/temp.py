131133bb1c00189c4b6c7cb12e623ca6a58a47f8
Spoon <aspoerndle@elon.edu>
2026-06-29 13:35:35 -0400

    Added functionality that should allow the robot to travel to the starting GPS cords from wherever the robot currently is. Not linked with MotorController, needs testing through console terminal.

--- RobotController/testFiles/MotorTester/temp.py
+++ RobotController/testFiles/MotorTester/temp.py
@@ -1,9 +1,4 @@
 import math
-
-import serial
-import time
-import adafruit_gps
- 
 start = [0,0]
 def GPStoCords(GPS):
     lat1,long1 = GPS
@@ -16,6 +11,7 @@ def GPStoCords(GPS):
     x = GPSWestEast([lat1,long1], [lat2,long2])
     y = GPSNorthSouth([lat1,long1], [lat2,long2])
     return x,y,distance
+
 def GPSWestEast(GPS_cords, start_cords):
     lat1, long1 = GPS_cords
     lat2,long2 = start_cords
@@ -32,8 +28,6 @@ def GPSNorthSouth(GPS_cords,start_cords):
     r = 111.32 #distance in km per degree of lat
     dis_north_south = lat2 - lat1 * r
     return dis_north_south
-
-
 def GPStoDistance(lat1,long1,lat2,long2):
     # Formula d=2*[7] radius_of_world * [6] arcsin( [5]sqrt( [1] sin**2(lat2-lat1/2) + [2] cos(lat1) * [3] cos(lat2) * [4] sin**2( long2-long1 / 2) )  )
     lat_diff = lat2 - lat1
@@ -53,24 +47,5 @@ def toRadians(num):
     num *= math.pi
     return num
 
-PORT = "/dev/ttyUSB0"      # change on mac/linux: e.g. "/dev/ttyUSB0" or "/dev/ttyACM0"
-BAUD = 9600         # common for NMEA on Adafruit Ultimate GPS
-
-#uart = busio.UART(TX, RX, baudrate=9600, timeout=30)
-uart = serial.Serial(PORT, baudrate=9600, timeout=3000)
-gps = adafruit_gps.GPS(uart, debug=False)
-while True:
-    gps.update()  # Update the GPS data
-    if gps.has_fix:  # Check if a fix is available
-        if(start[0] == 0):
-            start = gps.latitude,gps.longitude
-        prompt = input("Check dis from start?")
-        if(prompt == "y"):
-            x,y,dis = GPStoCords([gps.latitude,gps.longitude])
-            print(f"X: {x}, Y: {y}, Dis: {dis}, check: {math.sqrt(x ** 2 + y ** 2)}")
-        # print(f'Latitude: {gps.latitude:.6f} degrees')
-        # print(f'Longitude: {gps.longitude:.6f} degrees')
-    else:
-        print('Waiting for fix...')
-    time.sleep(1)  # Wait for a second before the next update
-
+x,y,dis = GPStoCords([.01,.01])
+print(f"X: {x}, Y: {y}, Dis: {dis}, check: {math.sqrt(x**2 + y**2)}")
