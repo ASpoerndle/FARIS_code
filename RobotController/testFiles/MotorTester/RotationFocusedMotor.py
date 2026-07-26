facc9a1dea47dce6a5cd93eb10120b6a9fd82f59
Spoon <aspoerndle@elon.edu>
2026-07-26 13:22:46 -0400

    rot

--- RobotController/testFiles/MotorTester/RotationFocusedMotor.py
+++ RobotController/testFiles/MotorTester/RotationFocusedMotor.py
@@ -7,13 +7,13 @@ class RotationFocusedMotor():
        speed = abs(speed)
        current = self.motor.encoder.getEncoderPosition()
 
-       forward = ((self.motor.forwardValue - 1) / 1023) * 360 % 360
+       # forward = ((self.motor.forwardValue - 1) / 1023) * 360 % 360
 
-       # current_degrees = ((current-1)/1023) * 360
-       current_degrees = ((current - 1) / 1023) * 360 % 360
 
-       target = (forward + angle) % 360
-
+       #current_degrees = ((current - 1) / 1023) * 360 % 360
+       current_degrees = self.getCurrentAngle()
+       # target = (forward + angle) % 360
+       target = angle
        speed *= 0.75
        error = (target - current_degrees + 180) % 360 - 180
        if (error > 90):
